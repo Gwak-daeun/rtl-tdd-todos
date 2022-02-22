acf8c6e37fb3e1dd06d0b4f227e3269bb1ad23fe
daeun <bbibibbibi22@gmail.com>
2022-02-22 02:46:56 +0000

    TodoForm Submit event test not ok

--- src/Tofoform.js
+++ src/Tofoform.js
@@ -1,12 +1,19 @@
 import React, { useCallback, useState } from "react";
 
-const Todoform = () => {
+const Todoform = ({onInsert}) => {
     const [value, setValue] = useState('');
     const onChange = useCallback(e => {
         setValue(e.target.value);
     }, []);
+
+    const onSubmit = useCallback(e => {
+        e.prevendDefault();
+        setValue('');
+        onInsert(value);
+    }, [onInsert, value]);
+
     return(
-        <form>
+        <form onSubmit={onSubmit}>
             <input placeholder="할 일을 입력하세요." value={value} onChange={onChange}/>
             <button type="submit">등록</button>
         </form>
